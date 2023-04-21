af90a82d5d62a61c51dcb3d0d53929356cb4bb25
RohitGhughtyal1 <91017826+RohitGhughtyal1@users.noreply.github.com>
2023-04-21 18:50:41 +0000

    profile is loading

--- app.js
+++ app.js
@@ -27,15 +27,16 @@ passport.deserializeUser(function (user, done) {
     done(null, user);
 });
 
+
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: process.env.GOOGLE_CALLBACK_URL,
-    passReqToCallback: true
+    callbackURL: process.env.GOOGLE_CALLBACK_URL
+    // passReqToCallback: true
 },
     function (accessToken, refreshToken, profile, done) {
         // You can add your own code here to handle user authentication and authorization
-        console.log(profile);
+        // console.log(profile);
         return done(null, profile);
     }
 ));
@@ -47,7 +48,12 @@ app.get('/', (req, res) => {
 });
 
 app.get('/profile', (req, res) => {
-    res.render('pages/profile.ejs');
+    // res.send("Logged In");
+    // console.log(req.user);
+    // console.log("hi");
+    // res.send(req.user.displayName);
+
+    res.render("pages/profile.ejs", {req: req});
 });
 
 
@@ -56,8 +62,8 @@ app.get('/google', passport.authenticate('google', { scope: ['email', 'profile']
 app.get('/google/callback', passport.authenticate('google', { failureRedirect: '/fail' }),
     (req, res) => {
         // Successful authentication, redirect to home page or do something else
-        console.log("hi");
-        // res.redirect('/profile');
+        // res.send(toString(res));
+        res.redirect('/profile');
     }
 );
 
